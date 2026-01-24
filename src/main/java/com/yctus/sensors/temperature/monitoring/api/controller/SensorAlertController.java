a04137b773ff729c1497a670c27cf721e6677e1e
michelangelomc <mickelchaves@hotmail.com>
2026-01-24 19:32:33 -0300

    requests - alerts - v1

--- src/main/java/com/yctus/sensors/temperature/monitoring/api/controller/SensorAlertController.java
+++ src/main/java/com/yctus/sensors/temperature/monitoring/api/controller/SensorAlertController.java
@@ -44,7 +44,6 @@ public class SensorAlertController {
                 .maxTemperature(sensorAlert.getMaxTemperature())
                 .minTemperature(sensorAlert.getMinTemperature())
                 .build();
-
     }
 
     @PutMapping
